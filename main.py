1431b41369e5220964e8b14f2769bd8321a85c33
TanishkG29 <142510813+TanishkG29@users.noreply.github.com>
2024-06-10 10:36:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,121 @@
 from tkinter import *
+from tkinter import font
 from tkinter import messagebox
-from random import choice, randint, shuffle
+import random
 import pyperclip
+
+CHARACTERS = "abcdef_gh!ijk_lmn$op_qr^stu_vxy*z1234567890!@#$^*("
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-#Password Generator Project
-def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+def gen_pwd():
+    import random
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+               'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+               'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    password_letters = [choice(letters) for _ in range(randint(8, 10))]
-    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
+    password_list = []
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    # password_letters = [char for char in letters[nr_letters]]
+    # password_symbols = [char for char in symbols[nr_symbols]]  # my password is getting generated as k%4
+    # password_numbers = [char for char in numbers[nr_numbers]]
+
+    # for char in range(nr_letters):
+    #     password_list.append(random.choice(letters))
+    #
+    # for char in range(nr_symbols):
+    #     password_list += random.choice(symbols)
+    #
+    # for char in range(nr_numbers):
+    #     password_list += random.choice(numbers)
     password_list = password_letters + password_symbols + password_numbers
-    shuffle(password_list)
+    random.shuffle(password_list)
+
+    #  join string method using function join
 
     password = "".join(password_list)
     password_entry.insert(0, password)
-    pyperclip.copy(password)
+    pyperclip.copy(password)   # save the password to the clipboard
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
 def save():
+    data_website_entry = website_entry.get()
+    data_email_entry = email_entry.get()
+    data_password_entry = password_entry.get()
 
-    website = website_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
-
-    if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+    if len(data_website_entry) == 0 or len(data_password_entry) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any of the fields empty")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                      f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        is_ok = messagebox.askokcancel(title=f"{data_website_entry}",
+                                       message=f"These are details entered:\nEmail:{data_email_entry} \nPassword: {data_password_entry}\nIs is ok?")
+
+        with open("data.txt", "a") as file:
+            file.write(f'{data_website_entry} | {data_email_entry} | {data_password_entry} \n ')
+        website_entry.delete(0, "end")
+        password_entry.delete(0, "end")
+
+        messagebox.showinfo(title="Success", message=f"Added {data_website_entry} to the data store")
 
 
-# ---------------------------- UI SETUP ------------------------------- #
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search_pwd():
+    pass
+
+
+# ------------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
-window.title("Password Manager")
-window.resizable(False, False)  # Not resizable in either axes.
+window.title("PASSWORD MANAGER")
+window.resizable(False, False)  # No scope of resizing the dimensions in either of the axes
 window.config(padx=50, pady=50)
 
-canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1)
+canvas = Canvas(width=200, height=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)  # width 200 and height 200--the dimensions of the image
+canvas.grid(column=1, row=0)
 
-#Labels
-website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
-password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
+list_fonts = list(font.families())
 
-#Entries
+website_label = Label(text="Website: ")
+website_label.grid(column=0, row=1)
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(column=1, row=1, columnspan=2)
 website_entry.focus()
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
-password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
 
-# Buttons
-generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+email_label = Label(text="E-Mail/Username:")
+email_label.grid(column=0, row=2)
+email_entry = Entry(window, width=35)
+email_entry.insert(0, "amber_m123@e-mail.com")
+email_entry.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+password_entry = Entry(width=35)
+password_entry.grid(column=1, row=3, columnspan=2)
+
+# how to get the entry box towards the label defining it
+
+generate_button = Button(text="Generate Password", bg="white", command=gen_pwd)
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=29, command=save, bg="white")
+add_button.grid(column=1, row=4, columnspan=2)
+
+# search_button = Button(text='Search', bg ='white', command=search_pwd())
 
 window.mainloop()
